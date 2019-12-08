94c343e44fe632efeb5b2719a40eb8fa95494398
Alex Birdeye <kingelessar@ukr.nwt>
2019-12-08 21:25:14 +0200

    Companies validation added.

---
+++ app/Http/Requests/UpdateCompany.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCompany extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'filled|string|max:255',
+            'email' => 'email:rfc,dns|unique:companies,id',
+            'logofile' => 'image'
+        ];
+    }
+}
